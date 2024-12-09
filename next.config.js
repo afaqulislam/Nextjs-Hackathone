5b8a088464e39a1cce2a525bfd3e1ce2fa53e945
Afaq ul Islam <afaqulislam707@gmail.com>
2024-12-09 22:48:18 +0500

    Hackathon Project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      // Add the domains of your external image hosts
+      domains: ['https://nextjs-hackathone-afaq1.vercel.app/'], // Replace with actual domain(s) if using external images
+    },
+  };
+  
+  module.exports = nextConfig;
+  
